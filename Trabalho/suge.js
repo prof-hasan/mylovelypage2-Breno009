f17aea439a9750eeed126bb2fa43ce5bbd398fd0
Breno009 <147253894+Breno009@users.noreply.github.com>
2025-09-04 19:03:56 -0300

    Delete Trabalho directory

--- Trabalho/suge.js
+++
@@ -1,5 +0,0 @@
-var botão = document.querySelector("button")
-botão.addEventListener('click', function () {
-    alert("Obrigado pela sua sujestão ou reclamação! Vamos fazer o possível para deixar o site no agrado de todos");
-}
-);

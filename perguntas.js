059175823a30ed47538537a0349f9c2e2cd07441
Breno009 <147253894+Breno009@users.noreply.github.com>
2025-09-04 19:05:47 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,34 @@
+export default [
+    {
+        pergunta: "Qual desses brawlers não consegue quebrar paredes com o Super?",
+        respostas: [
+            { opcao: "Edgar", correta: true },
+            { opcao: "Bull", correta: false },
+            { opcao: "Colt", correta: false },
+        ]
+    },
+    {
+        pergunta: "Quando a Emz usa seu Super, o que acontece?",
+        respostas: [
+            { opcao: "Congela os inimigos por 3 segundos", correta: false },
+            { opcao: "Recupera a vida dos aliados", correta: false },
+            { opcao: "Cria uma névoa que causa dano contínuo", correta: true },
+        ]
+    },
+    {
+        pergunta: "Qual era o nome do Rico antes dele ser remodelado?",
+        respostas: [
+            { opcao: "Ricochet", correta: true },
+            { opcao: "Ricocheta", correta: false },
+            { opcao: "Ricochete", correta: false },
+        ]
+    },
+    {
+        pergunta: "Qual foi a primeira skin a ser lançada no jogo?",
+        respostas: [
+            { opcao: "Shelly Estelar", correta: false },
+            { opcao: "Shelly Bandida", correta: true },
+            { opcao: "Nita Panda", correta: false },
+        ]
+    }
+];
